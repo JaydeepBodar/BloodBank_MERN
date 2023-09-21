dbeb9559ae920be84cf4a099687959fbe941b73f
JaydeepBodar <jaydeepbodar732@gmail.com>
2023-09-21 19:07:07 +0530

    anyalitic_add

--- client/src/Pages/Dashbordlist/Anaylitic.js
+++ client/src/Pages/Dashbordlist/Anaylitic.js
@@ -9,45 +9,65 @@ const Anaylitic = () => {
   const { data, loading } = useFetch(`${api}inventory/anaylitic`);
   return (
     <Layout>
-      <Chartdata totalblood={data?.bloodData} />
-      <div className={style.Anylatic_wrapper}>
-        <Row>
-          {data?.bloodData?.map((val, index) => {
-            const {
-              totalblood,
-              totanumberofRequestbloodIn,
-              totalnumberofOutblood,
-              blood,
-            } = val;
-            return (
-              <Col lg={3} md={4} sm={6} key={index} className={style.boxes_blood}>
-                <div className={style.Anaylitic_block}>
-                  <h4>{blood}</h4>
-                  <h6>
-                    Total in :-{" "}
-                    {!totanumberofRequestbloodIn[0]?.total ? (
-                      <span>0</span>
-                    ) : ( 
-                      totanumberofRequestbloodIn[0]?.total
-                    )}ML
-                  </h6>
-                  <h6>
-                    Total out :-{" "}
-                    {!totalnumberofOutblood[0]?.total ? (
-                      <span>0</span>
-                    ) : (
-                      totalnumberofOutblood[0]?.total
-                    )}ML
-                  </h6>
-                  <h6>
-                    Total Blood :- {!totalblood ? <span>0</span> : totalblood}ML
-                  </h6>
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
-      </div>
+      {loading && <p>Loading...</p>}
+      {!loading && data?.bloodData?.length === 0 && (
+        <div className={style.user_record}>
+          <h3>No Recoard Found Of Hospital</h3>
+        </div>
+      )}
+      {!loading && data?.bloodData?.length > 0 && (
+        <React.Fragment>
+          {" "}
+          <Chartdata totalblood={data?.bloodData} />
+          <div className={style.Anylatic_wrapper}>
+            <Row>
+              {data?.bloodData?.map((val, index) => {
+                const {
+                  totalblood,
+                  totanumberofRequestbloodIn,
+                  totalnumberofOutblood,
+                  blood,
+                } = val;
+                return (
+                  <Col
+                    lg={3}
+                    md={4}
+                    sm={6}
+                    key={index}
+                    className={style.boxes_blood}
+                  >
+                    <div className={style.Anaylitic_block}>
+                      <h4>{blood}</h4>
+                      <h6>
+                        Total in :-{" "}
+                        {!totanumberofRequestbloodIn[0]?.total ? (
+                          <span>0</span>
+                        ) : (
+                          totanumberofRequestbloodIn[0]?.total
+                        )}
+                        ML
+                      </h6>
+                      <h6>
+                        Total out :-{" "}
+                        {!totalnumberofOutblood[0]?.total ? (
+                          <span>0</span>
+                        ) : (
+                          totalnumberofOutblood[0]?.total
+                        )}
+                        ML
+                      </h6>
+                      <h6>
+                        Total Blood :-{" "}
+                        {!totalblood ? <span>0</span> : totalblood}ML
+                      </h6>
+                    </div>
+                  </Col>
+                );
+              })}
+            </Row>
+          </div>
+        </React.Fragment>
+      )}
     </Layout>
   );
 };
